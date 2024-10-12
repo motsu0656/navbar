456b46c2001c769a1bee9111a72d092fef0fc649
motsu0656 <yuta.sakamoto0506@gmail.com>
2024-10-12 23:21:39 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// ナビバーを取得
+const header = document.getElementById('main-header');
+
+// ヘッダーのオフセット位置（上からの距離）を取得
+const headerOffsetTop = header.offsetTop;
+
+// スクロールイベントを監視
+window.addEventListener('scroll', () => {
+  // ユーザーのスクロール位置がヘッダーのオフセット位置を超えたら固定
+  if (window.pageYOffset > headerOffsetTop) {
+    header.classList.add('fixed');
+  } else {
+    header.classList.remove('fixed');
+  }
+});
